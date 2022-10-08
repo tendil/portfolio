f2ceb3ddfaff2ef68cd6ed358564e26c90bbccfd
tendil <mejta.diman@gmail.com>
2022-10-08 06:27:09 +0000

    Remove old stuff

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hrj48*mobkzo)s6q+2wfqcg=_=@5c38n&sb4_vlp#bb^zyp_pi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['www.meyta.pp.ua']
 
 
 # Application definition
